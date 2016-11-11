e0843c09a938ff40e9c43209fb39e17caa40eac4
Gavrilov E <mr.jerik@gmail.com>
2016-11-12 01:21:33 +0300

    added lombok

--- core/src/com/darkun/entity/Player.java
+++ core/src/com/darkun/entity/Player.java
@@ -1,23 +1,20 @@
 package com.darkun.entity;
 
+
+import lombok.Getter;
+
 /**
  * @author Gavrilov E. <mr.jerik@gmail.com>
  * @since 10.11.16
  */
 public class Player {
+    @Getter
     private int health = 100;
+    @Getter
     private boolean gameOver = false;
 
     public void getDamage(int damage) {
         health -= damage;
         gameOver = health <= 0;
-    }
-
-    public boolean isGameOver() {
-        return gameOver;
-    }
-
-    public int getHealth() {
-        return health;
     }
 }
